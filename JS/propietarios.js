e4e4ad26bb5c18efb5965305f216b6f97e7bbcf5
“Jara888” <jujagu20@petalmail.com>
2022-10-27 16:05:10 -0500

    actualizacion del CRUD, eliminar, mostrar e insertar funcionan ok

--- JS/propietarios.js
+++ JS/propietarios.js
@@ -1,5 +1,5 @@
 // FUNCION PARA CONSULTAR UN PROPIETARIO
-function ConsultaPropietario ()
+function ConsultaPropietarios ()
 {
 var datos = {"Numero_Identificacion" : document.getElementById("Numero_Identificacion").value,
 }
@@ -7,7 +7,7 @@ console.log(datos)
 
 $.ajax({
         type: "post",
-        url: "http://localhost:3000/Consultar_Propietario",
+        url: "http://localhost:3000/Consultar_Propietarios",
         data : datos,
         dataType: "json",
         success: function (data) {
@@ -36,30 +36,30 @@ $.ajax({
 });
 }
 
-// FUNCION PARA CONSULTAR UN PROPIETARIO
+// FUNCION PARA CONSULTAR UN PROPIETARIO  --> no funciona ko
 function ListarUsers(){
 
-	var datos={}	
+    var datos={}    
 
 $.ajax({
-	type:"post",
-	url:"http://localhost:3003/MostrarUsuarios",
-	data:datos,
-	dataType:"json",
-	success:function(data){
-			if(data.result.length>0)
+    type:"post",
+    url:"http://localhost:3000/MostrarPropietario",
+    data:datos,
+    dataType:"json",
+    success:function(data){
+            if(data.result.length>0)
             { 
-            	let user=""
-            	let cant=0
+                let user=""
+                let cant=0
 
-            	for(i=0;i<data.result.length;i++)
+                for(i=0;i<data.result.length;i++)
                 {
                    $('#gm').remove()
                 }
-                  for(i=0;i<	data.result.length;i++)
+                  for(i=0;i<    data.result.length;i++)
                 {             
                     cant=cant+1                       
-                    user=cant +' '+'Nombre:'+' '+data.result[i].Nombre+' '+ data.result[i].Correo+' '+data.result[i].UserName+ " "+data.result[i].Id+'\n'
+                    user=cant +' '+'Numero_Identificacion:'+' '+data.result[i].Numero_Identificacion+' '+ data.result[i].Primer_Nombre+' '+data.result[i].Segundo_Nombre+' '+data.result[i].Primer_Apellido+' '+ data.result[i].Segundo_Apellido+' '+data.result[i].correo+' '+data.result[i].Telefono+' '+data.result[i].Direccion_Contacto+'\n'
                     $('#user').append('<p id="gm">'+user+'</p>')                                  
                 }   
             }
@@ -68,7 +68,7 @@ $.ajax({
 
                 alert('No hay registros de Usuarios')
             }
-	}
+    }
 })
 }
 
@@ -96,7 +96,7 @@ $.ajax({
         {
             console.log('Propietario Almacenado - OK')
             alert('El Propietario fue almacenado Satisfactoriamente')
-            location.href='../VISTAS/inmuebles.html'
+            location.href='../VISTAS/propietarios.html'
         }
         else
         {
@@ -107,78 +107,57 @@ $.ajax({
 })
 }
 
-// FUNCION PARA REGISTRAR UN PROPIETARIO
-/* function Agregar_Propietario()
-{
-	var datos={
-        "Direccion_Contacto":document.getElementById("Direccion_Contacto").value,
-        "Numero_Identificacion":document.getElementById("Numero_Identificacion").value,
-        "Primer_Apellido":document.getElementById("Primer_Apellido").value,
-		"Primer_Nombre":document.getElementById("Primer_Nombre").value,
-        "Segundo_Apellido":document.getElementById("Segundo_Apellido").value,
-		"Segundo_Nombre":document.getElementById("Segundo_Nombre").value,
-		"Telefono":document.getElementById("Telefono").value,
-        "correo":document.getElementById("correo").value
-	}
-console.log(datos)
-$.ajax({
-	type:"post",
-	url:"http://localhost:3000/Agregar_Propietarios",
-	data:datos,
-	dataType:"json",
-	success:function(data){
-		if (data.save == 1)
-		{
-			console.log('Propietario Almacenado - OK')
-			alert('El Propietario fue almacenado Satisfactoriamente')
-			location.href='../VISTAS/inmuebles.html'
-		}
-		else
-		{
-			console.log('Fatal Error - Propietario NO almacenado')
-			alert('Fatal Error - Propietario NO Almacenado')
-		}
-	}
-})
-} */
-
 // FUNCION PARA ACTUALIZAR UN PROPIETARIO
 function Actualizar(){
-	var datos={
-        "Direccion_Contacto":document.getElementById("Direccion_Contacto").value,
+
+    var datos={
+          "Numero_Identificacion":document.getElementById("Numero_Identificacion").value,
+        "Primer_Nombre":document.getElementById("Primer_Nombre").value,
+        "Segundo_Nombre":document.getElementById("Segundo_Nombre").value,
         "Primer_Apellido":document.getElementById("Primer_Apellido").value,
-		"Primer_Nombre":document.getElementById("Primer_Nombre").value,
         "Segundo_Apellido":document.getElementById("Segundo_Apellido").value,
-		"Segundo_Nombre":document.getElementById("Segundo_Nombre").value,
-		"Telefono":document.getElementById("Telefono").value,
-        "correo":document.getElementById("correo").value
-	}
-$.ajax({
-	type:"post",
-	url:"http://localhost:3000/Actualizar_Propietario",
-	data:datos,
-	dataType:"json",
-	success:function(data){
-		if(data.save == 1)
-		{
-			console.log('Propietario Actualizado Satisfactoriamente')
-			alert('Propietario Actualizado Satisfactoriamente')
-			location.href='../VISTAS/menu.html'
-		}
-		else
-		{
-			console.log('Error - Propietario NO Actualizado')
-			alert('Error - Propietario NO Actualizado')
-		}
-	}
-})
+        "correo":document.getElementById("correo").value,
+        "Telefono":document.getElementById("Telefono").value,
+        "Direccion_Contacto":document.getElementById("Direccion_Contacto").value
+
+
+
+    }
+    console.log(datos)
+    $.ajax({
+        type:"post",
+        url:"http://localhost:3000/update",
+        data:datos,
+        dataType:"json",
+        success:function(data){
+            if(data.save==1)
+            {
+                console.log('Usuario Actualizado Satisfactoriamente')
+                alert('Usuario Actualizado Satisfactoriamente')
+                location.href='./VISTAS/propietarios.html' //pagina donde será redireccionado tras actualizar o la función que tenga programado
+
+            }
+            else
+            {
+                console.log('Error')
+                alert('Error - Usuario No Actualizado Registrado')
+
+            }
+
+
+        }
+
+    })
 }
 
 // FUNCION PARA ELIMINAR UN PROPIETARIO  ---> funciona ok
 function Eliminar(){
+
     var datos={
-        "Numero_Identificacion":document.getElementById("Numero_Identificacion").value,        
+        "Numero_Identificacion":document.getElementById("Numero_Identificacion").value,     
     }
+
+    console.log(datos)
 $.ajax({
     type:"post",
     url:"http://localhost:3000/delete_propietarios",
@@ -190,39 +169,93 @@ $.ajax({
             console.log('Usuario Eliminado Satisfactoriamente')
             alert('Usuario Eliminado Satisfactoriamente')
             location.href='../VISTAS/propietarios.html'
+
         }
         else
         {
             console.log('Error')
             alert('Error - Usuario No Eliminado')
+
         }
+
+
     }
+
 })
+
 }
 
-/* FUNCION PARA ELIMINAR UN PROPIETARIO
-function Eliminar(){
+//para buscar por id de propietario ---> funciona hasta el back pero no se muestra en el front
 
-	var datos={
-		"Id":document.getElementById("Id").value,		
-	}
+function UserById(){
+
+    var datos={
+
+        "Numero_Identificacion":document.getElementById("Numero_Identificacion").value,
+
+
+    }
+    
+console.log(datos)
+
 $.ajax({
-	type:"post",
-	url:"http://localhost:3003/delete",
-	data:datos,
-	dataType:"json",
-	success:function(data){
-		if(data.eliminado==1)
-		{
-			console.log('Usuario Eliminado Satisfactoriamente')
-			alert('Usuario Eliminado Satisfactoriamente')
-			location.href='../index.html'
-		}
-		else
-		{
-			console.log('Error')
-			alert('Error - Usuario No Eliminado')
-		}
-	}
+    type:"post",
+    url:"http://localhost:3000/showuser",
+    data:datos,
+    dataType:"json",
+    success:function(data){
+    
+            if(data.result.length>0)
+            { 
+
+                let user=""
+                let cant=0
+
+                for(i=0;i<data.result.length;i++)
+                {
+
+                     $('#gm').remove();
+
+                }
+
+                for (i=0;i<data.result.length;i++)
+                {
+                    cant=cant+1         
+                    user= cant+' '+data.result[i].Numero_Identificacion+' '+data.result[i].Primer_Nombre+' '+ data.result[i].Segundo_Nombre+' '+data.result[i].Primer_Apellido+' '+data.result[i].Segundo_Apellido+' '+data.result[i].correo+' '+data.result[i].Telefono+' '+data.result[i].Direccion_Contacto+'\n'
+                    
+                     //$('#user').append('<p id="gm">'+user+'</p>') //esta etiqueta se usa para mostrar por por medio de etiqueta
+
+                      //con este código de más abajo se agregan los datos de la base de datos en los campos de la tabla propietarios
+
+                     document.getElementById("Numero_Identificacion").value = data.result[i].Numero_Identificacion
+                     document.getElementById("Primer_Nombre").value = data.result[i].Primer_Nombre
+                     document.getElementById("Segundo_Nombre").value = data.result[i].Segundo_Nombre
+                     document.getElementById("Primer_Apellido").value = data.result[i].Primer_Apellido
+                     document.getElementById("Segundo_Apellido").value = data.result[i].Segundo_Apellido
+                     document.getElementById("correo").value = data.result[i].correo
+                     document.getElementById("Telefono").value = data.result[i].Telefono
+                     document.getElementById("Direccion_Contacto").value = data.result[i].Direccion_Contacto
+
+
+
+
+
+
+                }
+            
+                     
+           console.log("todo hasta aqui")
+ 
+            }
+            else
+            {console.log('No hay registros de usuario')
+
+                alert('No hay registros de usuario')
+            }
+
+
+    }
+
 })
-}*/
+
+}
